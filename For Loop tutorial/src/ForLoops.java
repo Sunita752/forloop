956b48815ddefc38a8b5c60e88ab50c33fb88c17
Sunita752 <74800878+Sunita752@users.noreply.github.com>
2020-12-10 16:27:18 -0600

    Add files via upload

---
+++ For Loop tutorial/src/ForLoops.java
@@ -0,0 +1,18 @@
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello everyone");
+		
+	String firstname ="Sunita";
+    String lastname ="Khanal";
+    String Fullname = firstname +" " + lastname;
+    
+    System.out.println(Fullname);
+
+    
+  		 
+	}
+
+}
